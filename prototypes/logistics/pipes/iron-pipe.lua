17d5f842e46321a70e211ee95423cac7538d1188
henrikperden <henrik.perden@gmail.com>
2017-05-14 07:15:53 +0200

    iron pipe fix

--- prototypes/logistics/pipes/iron-pipe.lua
+++ prototypes/logistics/pipes/iron-pipe.lua
@@ -1,6 +1,4 @@
-data:extend(
-{
-{
+data:extend({
   {
     type = "item",
     name = "pipe",
@@ -11,64 +9,17 @@ data:extend(
     place_result = "pipe",
     stack_size = 5000
   },
-    {
+  {
     type = "recipe",
     name = "pipe",
     ingredients = {{"plumbing", 10}},
-	result_count = 5,
+    result_count = 5,
     result = "pipe"
   },
-  {
-    type = "pipe",
-    name = "pipe",
-    icon = "__base__/graphics/icons/pipe.png",
-    flags = {"placeable-neutral", "player-creation"},
-    minable = {hardness = 0.2, mining_time = 0.5, result = "pipe"},
-    max_health = 100,
-    corpse = "small-remnants",
-    resistances =
-    {
-      {
-        type = "fire",
-        percent = 80
-      },
-      {
-        type = "impact",
-        percent = 30
-      }
-    },
-    fast_replaceable_group = "pipe",
-    collision_box = {{-0.29, -0.29}, {0.29, 0.29}},
-    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-    fluid_box =
-    {
-      base_area = 1,
-      pipe_connections =
-      {
-        { position = {0, -1} },
-        { position = {1, 0} },
-        { position = {0, 1} },
-        { position = {-1, 0} }
-      },
-    },
-    pictures = pipepictures(),
-    working_sound =
-    {
-      sound = {
-        {
-          filename = "__base__/sound/pipe.ogg",
-          volume = 0.85
-        },
-      },
-      match_volume_to_activity = true,
-      max_sounds_per_type = 3
-    },
-
-    horizontal_window_bounding_box = {{-0.25, -0.28125}, {0.25, 0.15625}},
-    vertical_window_bounding_box = {{-0.28125, -0.5}, {0.03125, 0.125}}
-  },
+}) 
   --Underground Pipe--
-    {
+data:extend({  
+  {
     type = "item",
     name = "pipe-to-ground",
     icon = "__base__/graphics/icons/pipe-to-ground.png",
@@ -78,86 +29,15 @@ data:extend(
     place_result = "pipe-to-ground",
     stack_size = 500
   },
-    {
+  {
     type = "recipe",
     name = "pipe-to-ground",
     ingredients =
     {
-      {"pipe", 125},
+     {"pipe", 125},
 	   {"explosives-analogue", 5},
     },
     result_count = 10,
-    result = "pipe-to-ground"
+    result = "pipe-to-ground",
   },
-   {
-    type = "pipe-to-ground",
-    name = "pipe-to-ground",
-    icon = "__base__/graphics/icons/pipe-to-ground.png",
-    flags = {"placeable-neutral", "player-creation"},
-    minable = {hardness = 0.2, mining_time = 0.5, result = "pipe-to-ground"},
-    max_health = 50,
-    corpse = "small-remnants",
-    resistances =
-    {
-      {
-        type = "fire",
-        percent = 80
-      }
-    },
-    collision_box = {{-0.29, -0.29}, {0.29, 0.2}},
-    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-    fluid_box =
-    {
-      base_area = 1,
-      pipe_covers = pipecoverspictures(),
-      pipe_connections =
-      {
-        { position = {0, -1} },
-        {
-          position = {0, 1},
-          max_underground_distance = 25
-        }
-      },
-    },
-    underground_sprite =
-    {
-      filename = "__core__/graphics/arrows/underground-lines.png",
-      priority = "high",
-      width = 32,
-      height = 32
-    },
-    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-    pictures =
-    {
-      up =
-      {
-        filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-up.png",
-        priority = "high",
-        width = 44,
-        height = 32 --, shift = {0.10, -0.04}
-      },
-      down =
-      {
-        filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-down.png",
-        priority = "high",
-        width = 40,
-        height = 32 --, shift = {0.05, 0}
-      },
-      left =
-      {
-        filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-left.png",
-        priority = "high",
-        width = 32,
-        height = 42 --, shift = {-0.12, 0.1}
-      },
-      right =
-      {
-        filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-right.png",
-        priority = "high",
-        width = 32,
-        height = 40 --, shift = {0.1, 0.1}
-      },
-    }
-  },
-  }
-  )
+ })
